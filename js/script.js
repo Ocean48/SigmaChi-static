0f39ed91bce64996860b8069fc02604bfd0aef0b
Ocean <77823676+Ocean48@users.noreply.github.com>
2022-08-12 15:21:27 -0400

    added mode botton
    can change between light and dark mode

--- js/script.js
+++ js/script.js
@@ -8,3 +8,30 @@ function header_menu() {
         x.className = "topnav";
     }
 }
+
+// Get cookie
+var m = document.cookie;
+if (document.cookie === "") {
+    document.cookie = "css/style_light.css";
+    m = document.cookie
+}
+// Set style sheet on load
+function change_mode(sheet) {
+    document.getElementById("page_style").setAttribute("href", sheet);
+}
+
+// To change to dark mode
+function mode_change_dark() {
+    m = "css/style_dark.css";
+    document.cookie = "css/style_dark.css";
+    document.getElementById("page_style").setAttribute("href", "css/style_dark.css");
+}
+
+// To change to light mode
+function mode_change_light() {
+    m = "css/style_light.css";
+    document.cookie = "css/style_light.css";
+    document.getElementById("page_style").setAttribute("href", "css/style_light.css");
+}
+
+window.onload = change_mode(m);
